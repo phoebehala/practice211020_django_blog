73a42e11d8e4363828742d6db52f3b5004436dd9
Phoebe <phyuhala@gmail.com>
2021-10-27 15:40:06 +0800

    Saving Users

--- src/djangoautic/accounts/views.py
+++ src/djangoautic/accounts/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-
+ 
 def signup_view(request):
-
-    #UserCreationForm() >>> create a new instance of this form
-    form = UserCreationForm()
-
-    #{key, value} >>> the dictionary we can send down
-    return render(request, 'accounts/signup.html', {'myForm':form})
+ 
+   if request.method == 'POST':
+ 
+       # take data and somehow validate
+       form = UserCreationForm(request.POST)
+      
+       if form.is_valid():  # true >>> valid
+           form.save()  # save data to the database
+           return redirect('articlesApp:list')   # return to article app and url named 'list' in the urls.py of articles app
+  
+   else:
+ 
+       #UserCreationForm() >>> create a new instance of this form
+       form = UserCreationForm()
+ 
+   #{key, value} >>> the dictionary we can send down
+   return render(request, 'accounts/signup.html', {'myForm':form})
