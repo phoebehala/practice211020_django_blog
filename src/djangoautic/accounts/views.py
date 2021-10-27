7795c06d77c525ff5da9f10c11cf6301465561ef
Phoebe <phyuhala@gmail.com>
2021-10-27 14:00:03 +0800

    User Creation Form

--- src/djangoautic/accounts/views.py
+++ src/djangoautic/accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+
+    #UserCreationForm() >>> create a new instance of this form
+    form = UserCreationForm()
+
+    #{key, value} >>> the dictionary we can send down
+    return render(request, 'accounts/signup.html', {'myForm':form})
