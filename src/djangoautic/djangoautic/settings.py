1e2c62ced613ce6816d5cf61fce6d4d419f9108e
Phoebe <phyuhala@gmail.com>
2021-10-29 17:16:22 -0700

    set DEBUG is False

--- src/djangoautic/djangoautic/settings.py
+++ src/djangoautic/djangoautic/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b&nj&+&(@2hx@qy^)3(9ct!k4htj(t^a-z74_(esyp!qgypiy9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
